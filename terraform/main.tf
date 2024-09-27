3303d78f50d9f9511033783dcfbc92a7c4d0d255
basil1987 <basil1987@gmail.com>
2024-09-27 21:12:59 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,7 +44,7 @@ resource "aws_security_group" "webservers" {
 
 resource "aws_instance" "web001" {
   count = "2"
-  ami           = "ami-0851b76e8b1bce90b"
+  ami           = "ami-0e86e20dae9224db8"
   instance_type = "t2.micro"
   vpc_security_group_ids = [ aws_security_group.webservers.id ]
   key_name = "jenkins"

effa2e16ab59707fdfcc73df7d9c0c099b22ec77
basil1987 <basil1987@gmail.com>
2024-09-27 21:11:23 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "ap-south-1"
+  region  = "us-east-1"
 }
 
 resource "aws_security_group" "webservers" {
@@ -47,7 +47,7 @@ resource "aws_instance" "web001" {
   ami           = "ami-0851b76e8b1bce90b"
   instance_type = "t2.micro"
   vpc_security_group_ids = [ aws_security_group.webservers.id ]
-  key_name = "priceline-ansible-demo"
+  key_name = "jenkins"
   tags = {
     Name = "Web-${count.index + 1}"
     type = "webserver"
